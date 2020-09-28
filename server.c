4475c610bf0903647e9335e4317450d0cbabd356
Azmah-Bad <56384031+Azmah-Bad@users.noreply.github.com>
2020-09-28 15:39:20 +0200

    fixed the server and better naming variables
    Signed-off-by: Azmah-Bad <56384031+Azmah-Bad@users.noreply.github.com>

--- server.c
+++ server.c
@@ -1,50 +1,69 @@
 #include "server.h"
+#define PORT 5000
 
 int main(int argc, char const *argv[])
 {
+    // initialize variables
+    int mSocket, mConnection, clientLength;
+    struct sockaddr_in serverAddress, clientAddress;
+
     //create socket
-    int s = socket(AF_INET, SOCK_STREAM, 0);
+    mSocket = socket(AF_INET, SOCK_STREAM, 0);
 
     // verify the socket creation worked
-    if (s < 0)
+    if (mSocket < 0)
     {
-        printf("error when creating socket");
+        printf("error when creating socket...\n");
         return 1;
     }
+    else
+        printf("socket created with success...\n");
 
+    // makes the port reuseable
     int reuse = 1;
-    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+    setsockopt(mSocket, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
 
-    struct sockaddr_in my_addr;
-    memset((char *)&my_addr, 0, sizeof(my_addr));
-    my_addr.sin_port = htons(5000);
-    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    my_addr.sin_family = AF_INET;
-    struct sockaddr_in p_addr;
+    // socketAddress
+    // struct sockaddr_in my_addr;
+    memset((char *)&serverAddress, 0, sizeof(serverAddress));
+    // assign IP, PORT
+    serverAddress.sin_port = htons(PORT);
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddress.sin_family = AF_INET;
+    // struct sockaddr_in p_addr;
 
     //p_addr = &my_addr;
 
     //bind
-    int bindValue = bind(s, ((struct sockaddr* ) &my_addr), sizeof(my_addr));
+    int bindValue = bind(mSocket, ((struct sockaddr *)&serverAddress), sizeof(serverAddress));
 
+    if (bindValue != 0)
+    {
+        printf("socket bind failed...\n");
+        return 1;
+    }
+    else
+    {
+        printf("socket binded successfully...\n");
+    }
 
-
-    
     //listen
-    int listenValue = listen(s,5);
+    if ((listen(mSocket, 5)) != 0)
+    {
+        printf("Listen failed...\n");
+        return 1;
+    }
+    else
+        printf("Server listening..\n");
 
-    socklen_t socketLen = sizeof(p_addr);
-
+    clientLength = sizeof(clientAddress);
 
     //accept
     while (1)
-    {   
-        if(listenValue < 0){
-            return 1;
-        }
-        printf("listnening on port 5000\n");
-        int acceptValue = accept(s, (struct sockaddr *) p_addr, &socketLen);
-        printf("valeur de accept %d \n", acceptValue);
+    {
+        printf("listnening on port 5000...\n");
+        mConnection = accept(mSocket, (struct sockaddr *)&clientAddress, &clientLength);
+        printf("valeur de accept %d \n", mConnection);
     }
 
     return 0;
